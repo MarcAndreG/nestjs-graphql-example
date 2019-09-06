90999a016b412b09d2b267d181d30ba9039f2b7e
Marc-Andre G <MarcAndreG@users.noreply.github.com>
2019-09-06 15:40:01 -0400

    Update books.resolver.ts

--- src/books/books.resolver.ts
+++ src/books/books.resolver.ts
@@ -2,10 +2,6 @@ import { Resolver, Query } from '@nestjs/graphql';
 
 @Resolver()
 export class BooksResolver {
-  // constructor(
-  //   private readonly authorsService: AuthorsService,
-  //   private readonly postsService: PostsService,
-  // ) {}
 
   @Query(() => String)
   async hello() {
